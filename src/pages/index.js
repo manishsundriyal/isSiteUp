256a2f318f4d7402c62a7cbeb0ff464a15d1a156
manishsundriyal <msundriyal27@gmail.com>
2021-10-17 00:05:12 +0530

    basic FE structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,184 +1,124 @@
-import * as React from "react"
+import React, { useState } from "react";
+import { SearchButton } from "../components";
 
 // styles
 const pageStyles = {
+  height: "100%",
   color: "#232129",
   padding: 96,
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
+  textAlign: "center",
+};
 const headingStyles = {
   marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
+};
 const headingAccentStyles = {
   color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
+};
 
 const descriptionStyle = {
   color: "#232129",
   fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
   lineHeight: 1.25,
-}
+};
 
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
+const searchBoxStyles = {
   position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
+  marginTop: "6rem",
+  marginBottom: "5rem",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+};
 
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
+const inputStyles = {
+  width: "23vw",
+  padding: "20px 38px",
+  outline: "none",
+  border: "2px solid #5e738d4d",
+  borderRadius: "10rem",
+  fontSize: "1.3rem",
+  fontFamily: "Mulish, sans-serif",
+  color: "#232129",
+  backgroundColor: "rgba(243, 248, 254, 0.4)",
+};
 
-// markup
+
 const IndexPage = () => {
+  const [url, setUrl] = useState("");
+  const [siteInfo, setSiteInfo] = useState(null);
+  const [email, setEmail] = useState("");
+
+  const onSearch = cb => {
+    // TODO: add url / ip address validations here
+    console.log("--search handler---", url);
+    setTimeout(() => {
+      setSiteInfo({
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      });
+      cb();
+    }, 1000);
+  };
+
+  const handleEmailNotify = () => {
+    // TODO: handle email validations
+    console.log("--email notify handler---", email);
+  }
+
   return (
     <main style={pageStyles}>
       <title>Home Page</title>
       <h1 style={headingStyles}>
-        Congratulations
+        Is Site Up?
         <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
       </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
+      <h2>
+        <span style={headingAccentStyles}>
+          — get notified when your favourite site is up?{" "}
         </span>
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
+        <span role="img" aria-label="Party popper emojis">
+          ☝️
+        </span>
+      </h2>
+      {siteInfo ? (
+        <React.Fragment>
+          <p>{siteInfo.description}</p>
+          <p>{siteInfo.description}</p>
+          <p>{siteInfo.description}</p>
+          <p>{siteInfo.description}</p>
+          <h4 style={ {marginTop: "4rem"}}>Notify me when site is up</h4>
+          <input
+              style={inputStyles}
+              placeholder="youremail@address.com"
+              type="email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+          <SearchButton onSearch={handleEmailNotify} />
+        </React.Fragment>
+      ) : (
+        <React.Fragment>
+          <div style={searchBoxStyles}>
+            <input
+              style={inputStyles}
+              placeholder="www.google.com"
+              value={url}
+              onChange={(event) => setUrl(event.target.value)}
+            />
+            <SearchButton onSearch={onSearch} />
+          </div>
+          <p style={descriptionStyle}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+            velit esse cillum dolore eu fugiat nulla pariatur.
+          </p>
+        </React.Fragment>
+      )}
     </main>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
